56dd4815263699c14d70b7704340f848691fd5b9
alecarlojesus <profalexandre.jesus@fiap.com.br>
2023-10-18 11:08:33 -0300

    Rotas dinâmicas com parâmetros.

---
+++ src/app/produtos/mosfet/[slug]/page.jsx
@@ -0,0 +1,10 @@
+
+export default function ProdutoSlug({params}) {
+  return (
+    <div>
+        <h1>Produto SLUG</h1>
+        <p>Slug: {params.slug}</p>
+    </div>
+  )
+}
+
---
+++ src/app/produtos/tocas/[id]/page.jsx
@@ -0,0 +1,11 @@
+
+export default function ProdutoId({params}) {
+    return (
+      <div>
+          <h1>Produto ID</h1>
+          <p>Slug: {params.id}</p>
+      </div>
+    )
+  }
+  
+  
