afee42b3106910c2e4fbfe5461edfff89735bc33
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-19 12:10:26 -0300

    Ajustando estilos no globals.css, inserindo um reset. Criando layers no globals.css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
+      <body className="reset">
         <Cabecalho />
           {children}
         <Rodape />
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div>
-        <h1>OOPS! 404!</h1>
+    <div className="bg-red-500 font-extrabold fixed w-full h-full">
+        <h1 className="text-center">OOPS! 404!</h1>
         <div>
           <figure>
-            <Image src="https://http.cat/images/404.jpg" alt="Gatp no jornal" width={750} height={600} />
+            <Image  className="block m-auto w-80" src="https://http.cat/images/404.jpg" alt="Gato no jornal" width={750} height={600} />
           </figure>
         </div>
-        <p>Página não encontrada!</p>
+        <p className="text-center">Página não encontrada!</p>
 
     </div>
   )
